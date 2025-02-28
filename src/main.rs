2bcdf6add11d4bd8fe3c22e78447fef781535e2a
Max Gilmour <mgilmour@maxgmr.ca>
2025-02-27 21:03:17 -0800

    freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// Entry point.
+///
+/// Use Linux conventions- make sure it's called `_start`
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// Called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // ...loop forever...
+    loop {}
 }
