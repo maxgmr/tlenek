a987f151f1ac18c35375cecabbfec4c7dafed80f
Max Gilmour <mgilmour@maxgmr.ca>
2025-03-01 23:09:08 -0800

    dummy exception handlers

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -7,7 +7,7 @@ use spin;
 use x86_64::{
     instructions::port::Port,
     registers,
-    structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode},
 };
 
 use crate::{
@@ -53,13 +53,31 @@ lazy_static! {
 
         idt.divide_error.set_handler_fn(divide_error_handler);
         idt.debug.set_handler_fn(debug_handler);
+        idt.non_maskable_interrupt.set_handler_fn(non_maskable_interrupt_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.overflow.set_handler_fn(overflow_handler);
+        idt.bound_range_exceeded.set_handler_fn(bound_range_exceeded_handler);
+        idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
+        idt.device_not_available.set_handler_fn(device_not_available_handler);
         // UNSAFE: This is safe because `DOUBLE_FAULT_IST_INDEX` is valid and not already used for
         // another exception.
         unsafe {
         idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(DOUBLE_FAULT_IST_INDEX);
         }
+        idt.invalid_tss.set_handler_fn(invalid_tss_handler);
+        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
+        idt.stack_segment_fault.set_handler_fn(stack_segment_fault_handler);
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.x87_floating_point.set_handler_fn(x87_floating_point_handler);
+        idt.alignment_check.set_handler_fn(alignment_check_handler);
         idt.machine_check.set_handler_fn(machine_check_handler);
+        idt.simd_floating_point.set_handler_fn(simd_floating_point_handler);
+        idt.virtualization.set_handler_fn(virtualization_handler);
+        idt.cp_protection_exception.set_handler_fn(cp_protection_exception_handler);
+        idt.hv_injection_exception.set_handler_fn(hv_injection_exception_handler);
+        idt.vmm_communication_exception.set_handler_fn(vmm_communication_exception_handler);
+        idt.security_exception.set_handler_fn(security_exception_handler);
 
         // PIC hardware interrupts
         idt[Timer.into()].set_handler_fn(timer_interrupt_handler);
@@ -82,6 +100,7 @@ pub fn init_idt() {
 
 /// Handler for division error.
 extern "x86-interrupt" fn divide_error_handler(stack_frame: InterruptStackFrame) {
+    // TODO
     generic_fault_handler(stack_frame, "DIVIDE ERROR");
 }
 
@@ -154,10 +173,39 @@ extern "x86-interrupt" fn debug_handler(stack_frame: InterruptStackFrame) {
     }
 }
 
+/// Handler for non-maskable interrupts.
+extern "x86-interrupt" fn non_maskable_interrupt_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_trap_handler(stack_frame, "NON-MASKABLE INTERRUPT");
+}
+
 /// Handler for breakpoints.
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-    exception_title();
-    println!("BREAKPOINT\n{:#?}", stack_frame);
+    generic_trap_handler(stack_frame, "BREAKPOINT");
+}
+
+/// Handler for overflows.
+extern "x86-interrupt" fn overflow_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_trap_handler(stack_frame, "OVERFLOW");
+}
+
+/// Handler for exceeding bound range.
+extern "x86-interrupt" fn bound_range_exceeded_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "BOUND RANGE EXCEEDED");
+}
+
+/// Handler for invalid opcode faults.
+extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "INVALID OPCODE");
+}
+
+/// Handler for when an FPU instruction is attempted but there is no FPU.
+extern "x86-interrupt" fn device_not_available_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "DEVICE NOT AVAILABLE");
 }
 
 /// Handler for double faults. Invoked when the CPU fails to invoke an exception handler. Used to
@@ -172,11 +220,152 @@ extern "x86-interrupt" fn double_fault_handler(
     panic!("DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
+/// Handler for invalid TSS.
+extern "x86-interrupt" fn invalid_tss_handler(stack_frame: InterruptStackFrame, error_code: u64) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("INVALID TSS {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for segment not present exceptions.
+extern "x86-interrupt" fn segment_not_present_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("SEGMENT NOT PRESENT {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for stack-segment faults.
+extern "x86-interrupt" fn stack_segment_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("STACK-SEGMENT FAULT {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for general protection faults.
+extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!(
+            "GENERAL PROTECTION FAULT {:#X}\n{:#?}",
+            error_code, stack_frame
+        );
+    }
+}
+
+/// Handler for page faults.
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("PAGE FAULT {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for x87 floating-point exceptions.
+extern "x86-interrupt" fn x87_floating_point_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "x87 FLOATING-POINT");
+}
+
+/// Handler for alignment check exceptions.
+extern "x86-interrupt" fn alignment_check_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("ALIGNMENT CHECK {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
 /// Handler for machine check. Unrecoverable- invoked when the processor detects internal errors
 /// (bad memory, bus errors, cache errors, etc.).
 extern "x86-interrupt" fn machine_check_handler(stack_frame: InterruptStackFrame) -> ! {
     exception_title();
     panic!("MACHINE_CHECK\n{:#?}", stack_frame)
+}
+
+/// Handler for SIMD floating-point exceptions.
+extern "x86-interrupt" fn simd_floating_point_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "SIMD FLOATING-POINT");
+}
+
+/// Handler for virtualization exceptions.
+extern "x86-interrupt" fn virtualization_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "VIRTUALIZATION");
+}
+
+/// Handler for control protection exceptions.
+extern "x86-interrupt" fn cp_protection_exception_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("CONTROL PROTECTION {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for hypervisor injection exceptions
+extern "x86-interrupt" fn hv_injection_exception_handler(stack_frame: InterruptStackFrame) {
+    // TODO
+    generic_fault_handler(stack_frame, "HYPERVISOR INJECTION");
+}
+
+/// Handler for VMM communication exceptions.
+extern "x86-interrupt" fn vmm_communication_exception_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("VMM COMMUNICATION {:#X}\n{:#?}", error_code, stack_frame);
+    }
+}
+
+/// Handler for security exceptions.
+extern "x86-interrupt" fn security_exception_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    // TODO
+    #[cfg(debug_assertions)]
+    {
+        exception_title();
+        println!("SECURITY {:#X}\n{:#?}", error_code, stack_frame);
+    }
 }
 
 /// Generic behaviour for fault errors (print exception on debug only, else do nothing)
@@ -245,7 +434,7 @@ unsafe fn send_eoi(interrupt_index: InterruptIndex) {
 fn exception_title() {
     let old_fg = vga_fg();
     set_vga_fg(VgaFgColour::LightRed);
-    print!("EXCEPTION");
+    print!("\nEXCEPTION");
     set_vga_fg(old_fg);
     print!(": ");
 }
