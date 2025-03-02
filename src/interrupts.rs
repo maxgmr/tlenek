fcaf82d0a2379b22bfb35b2cc029b180d00882c2
Max Gilmour <mgilmour@maxgmr.ca>
2025-03-01 17:20:33 -0800

    impl keyboard interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,15 +1,21 @@
 //! Functionality related to interrupts.
 
 use lazy_static::lazy_static;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use pic8259::ChainedPics;
 use spin;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::{
+    instructions::port::Port,
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+};
 
 use crate::{
     gdt::DOUBLE_FAULT_IST_INDEX,
     print, println,
     vga_text::{set_vga_fg, vga_fg, VgaFgColour},
 };
+
+const PS2_CONTROLLER_PORT: u16 = 0x60;
 
 const PIC_INTERRUPT_LINES: u8 = 8;
 
@@ -24,6 +30,8 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + PIC_INTERRUPT_LINES;
 pub enum InterruptIndex {
     /// Programmable interval timer (PIT) interrupt
     Timer = PIC_1_OFFSET,
+    /// Keyboard interrupt
+    Keyboard,
 }
 impl From<InterruptIndex> for u8 {
     fn from(value: InterruptIndex) -> Self {
@@ -38,6 +46,8 @@ impl From<InterruptIndex> for usize {
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
+        use InterruptIndex::*;
+
         let mut idt = InterruptDescriptorTable::new();
 
         idt.breakpoint.set_handler_fn(breakpoint_handler);
@@ -49,7 +59,8 @@ lazy_static! {
         idt.machine_check.set_handler_fn(machine_check_handler);
 
         // PIC hardware interrupts
-        idt[InterruptIndex::Timer.into()].set_handler_fn(timer_interrupt_handler);
+        idt[Timer.into()].set_handler_fn(timer_interrupt_handler);
+        idt[Keyboard.into()].set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
@@ -93,14 +104,48 @@ extern "x86-interrupt" fn machine_check_handler(stack_frame: InterruptStackFrame
 
 /// Handler for the hardware timer interrupt.
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
-    // Send EOI signal
-    // UNSAFE: Using the wrong interrupt vector number could delete an important unsent interrupt
-    // or cause the system to hang.
     unsafe {
-        PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Timer.into());
+        send_eoi(InterruptIndex::Timer);
     }
+}
+
+/// Handler for the hardware keyboard interrupt.
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    lazy_static! {
+        static ref KEYBOARD: spin::Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            spin::Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+
+    // Read scancode
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(PS2_CONTROLLER_PORT);
+    let scancode: u8 = unsafe { port.read() };
+
+    // Print key
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                // DecodedKey::RawKey(key) => print!("{:?}", key),
+                DecodedKey::RawKey(_) => (),
+            }
+        }
+    }
+
+    unsafe {
+        send_eoi(InterruptIndex::Keyboard);
+    }
+}
+
+/// Send end of interrupt signal
+/// UNSAFE: Using the wrong interrupt vector number could delete an important unsent interrupt
+/// or cause the system to hang.
+unsafe fn send_eoi(interrupt_index: InterruptIndex) {
+    PICS.lock().notify_end_of_interrupt(interrupt_index.into());
 }
 
 /// Print "EXCEPTION" in nice scary red text.
